fba5e8e45293af94026de894d37f7f55e2801b46
GabrielQZ <gabrielquinn.photography@gmail.com>
2024-02-19 17:46:04 -0500

    final before pics

--- app.js
+++ app.js
@@ -86,9 +86,12 @@ function countNeighbors(x, y) {
   let count = 0;
   for (let i = -1; i <= 1; i++) {
     for (let j = -1; j <= 1; j++) {
-      if (i === 0 && j === 0) continue; // Skip the current cell
-      const row = (x + i + numRows) % numRows; // Handle edge wrapping
-      const col = (y + j + numCols) % numCols; // Handle edge wrapping
+      if (i === 0 && j === 0) continue; // skip the current cell that is having its neighbors counted
+      // handle edge wrapping for left/right boundary
+      const row = (x + i + numRows) % numRows; 
+      // handle edge wrapping for top/bottom boundary
+      const col = (y + j + numCols) % numCols; 
+      // if the cell being looked at is alive add one to the count
       count += globalGameState.state[row][col].alive;
     }
   }
@@ -103,9 +106,16 @@ function calculateNextGameState() {
       const neighbors = countNeighbors(i, j);
       const currentCell = globalGameState.state[i][j];
       if (currentCell.alive) {
-        newRow.push({ alive: neighbors === 2 || neighbors === 3 ? 1 : 0, age: neighbors === 2 || neighbors === 3 ? currentCell.age + 1 : 0 });
+        newRow.push({ 
+          alive: neighbors === 2 || neighbors === 3 ? 1 : 0, 
+          age: neighbors === 2 || neighbors === 3 ? 
+          currentCell.age + 1 : 0 
+        });
       } else {
-        newRow.push({ alive: neighbors === 3 ? 1 : 0, age: neighbors === 3 ? 1 : 0 });
+        newRow.push({ 
+          alive: neighbors === 3 ? 1 : 0, 
+          age: neighbors === 3 ? 1 : 0 
+        });
       }
     }
     nextGameState.push(newRow);
@@ -118,7 +128,7 @@ function calculateNextGameState() {
   };
 }
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3333;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}\nhttp://localhost:${PORT}\n\n`);
   initializeGame();
